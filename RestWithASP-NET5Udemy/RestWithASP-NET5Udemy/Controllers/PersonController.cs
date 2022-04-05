8ce6cfdafbe11f20e7ed769ce81384a072f47207
Tiago Cerqueira <cerqueira.tiago15@gmail.com>
2022-04-05 14:41:01 -0300

    working with 4 verbs

---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace RestWithASP_NET5Udemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult GetSum(string firstNumber, string secondNumber)
+        {
+            if (decimal.TryParse(firstNumber, out decimal number1) && decimal.TryParse(secondNumber, out decimal number2))
+            {
+                decimal sum = number1 + number2;
+                return Ok(sum);
+            }
+
+            return BadRequest("Invalid input");
+        }
+    }
+}
---
+++ RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace RestWithASP_NET5Udemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
+    }
+}
